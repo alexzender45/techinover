ccebe157e372d04edbe070f609b44fc0188e8057
samuelAzurestech <abiokorigho.samuel@azurestech.com>
2021-10-20 07:43:15 +0100

    Complete Project

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ baseRoute.get("/", (req, res) =>
   res
     .status(200)
     .send(
-      '<code>Techinover Running...<a target="_blank" href="https://documenter.getpostman.com/view/7896471/TzsikPQV" style="text-decoration: none; cursor: pointer; color: black; font-weight: bold">&lt;Go To Docs/&gt;</a></code>'
+      '<code>Techinover Running...<a target="_blank" href="https://documenter.getpostman.com/view/7896471/UV5XjHsr" style="text-decoration: none; cursor: pointer; color: black; font-weight: bold">&lt;Go To Docs/&gt;</a></code>'
     )
 );
 
